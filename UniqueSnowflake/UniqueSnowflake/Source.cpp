09c7ecfcf03ee8f371e353612fc8b63dea069161
Travisrowe <travisrowe18@gmail.com>
2018-10-18 13:36:14 -0500

    UniqueSnowflakes

--- UniqueSnowflake/UniqueSnowflake/Source.cpp
+++ UniqueSnowflake/UniqueSnowflake/Source.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
+typedef unordered_map<int, bool>::iterator mapIter;
 
 int main()
 {
-	int T, N;
+	int T, n, snowflake;
 	cin >> T;
 	for (int i = 0; i < T; i++)
 	{
-		cin >> N;
-		for (int j = 0; j < N; j++)
+		cin >> n;
+		unordered_map<int, bool> myMap(n * 2); //pre-allocate double the necessary size
+		int greatestCount = 0;
+		for (int j = 0; j < n; j++)
 		{
-
+			cin >> snowflake;
+			int count = 0;
+			mapIter iter;
+			iter = myMap.find(snowflake);
+			if (iter != myMap.end()) //snowflake was found
+			{
+				//erase all values before and including iter
+				mapIter newStart = myMap.begin();
+				iter++; //increment iter so the clone gets erased
+				while (newStart != iter)
+				{
+					mapIter temp = newStart;
+					newStart++;
+					myMap.erase(temp);
+				}
+			}
+			myMap[snowflake] = true;
+			//set greatestCount
+			if (myMap.size() > greatestCount)
+				greatestCount = myMap.size();
 		}
+		cout << greatestCount << '\n';
 	}
 }
