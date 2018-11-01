33fc02e2e4ad302d76bcc58ba16363620b8404e4
travisrowe <travisrowe18@gmail.com>
2018-10-31 22:05:22 -0500

    factors

--- FactorsAndFactorials/FactorsAndFactorials/Source.cpp
+++ FactorsAndFactorials/FactorsAndFactorials/Source.cpp
@@ -29,17 +29,48 @@ void SetSieveOfEratosthenes(set<int> &primes)
 int main()
 {
 	int m, n;
-	set<int> primes; //list of prime numbers between up to 1,000,000
+	set<int> primes; //list of prime numbers up to 1,000,000
 	SetSieveOfEratosthenes(primes);
 	
-	//map that stores number of prime numbers whose product equals the key
-	unordered_map<int, vector<int>> primeMultiples;
+	//stores number of prime numbers whose product equals the key
+	vector<int> primeMultiples;
 	cin >> n;
 	while (n != 0)
 	{
-		m = n - 1;
-
-
+		primeMultiples.assign(15, 0); //fill vector with 15 0s
+		m = n;
+		while (m > 1)
+		{
+			set<int>::iterator iter = primes.find(m);
+			if (iter != primes.end()) // m is a prime number
+			{
+				//index that iter is pointing to
+				int index = distance(primes.begin(), iter); 
+				primeMultiples[index]++;
+				m--;
+			}
+			else //m is a multiple of a prime number, we must find that number
+			{
+				set<int>::iterator it = primes.begin();
+				int o = m % (int)*it;
+				while (o != 0 && it != primes.end())
+				{
+					it++;
+					o = m % (int)*it;
+				} //the prime which m is divisible by has been found
+				int index = distance(primes.begin(), it);
+				primeMultiples[index]++;
+				m = m / (int)*it;
+			}
+		}
+		cout.width(3);
+		cout << right << n << "! = ";
+		for (auto i : primeMultiples)
+		{
+			cout.width(3);
+			cout << right << primeMultiples[i];
+		}
+		cout << '\n';
 		cin >> n;
 	}
 }
