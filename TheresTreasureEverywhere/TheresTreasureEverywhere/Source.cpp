675c7b98e1567fd9d6f880f01dc17eefcfc62649
Travisrowe <travisrowe18@gmail.com>
2018-11-05 22:13:03 -0600

    files

--- TheresTreasureEverywhere/TheresTreasureEverywhere/Source.cpp
+++ TheresTreasureEverywhere/TheresTreasureEverywhere/Source.cpp
@@ -8,25 +8,54 @@ using namespace std;
 int main()
 {
 	string input;
+	int mapNum = 1;
 	const string DELIM = ",";
-	getline(cin, input); //input is the full line
+	getline(cin, input, '.'); //input is the full line minus the period
 	while (input != "END")
 	{
 		size_t pos = input.find(DELIM);
 		double x = 0;
 		double y = 0;
-		while (pos != 0)
+		double mag; //magnitude
+		string dir; //direction
+		while (input != "")
 		{
 			stringstream ss;
 			string token = input.substr(0, pos);
-			input = input.substr(pos + 1);
+			if (pos + 1 < input.length())
+				input = input.substr(pos + 1);
+			else //this is the last direction
+				input = "";
 			ss << token;
-			int mag; //magnitude
-			string dir; //direction
+			
 			ss >> mag >> dir;
 
-			cout << mag << dir << endl;
-		}
-		getline(cin, input);
+			if (dir.length() > 1) //45 degree cardinal direction
+				mag = sqrt((mag * mag) / 2); //Pytharogm's theorem
+			for (int i = 0; i < dir.length(); i++)
+			{
+				switch (dir[i])
+				{
+				case 'N':
+					y += mag;
+					break;
+				case 'S':
+					y -= mag;
+					break;
+				case 'W':
+					x -= mag;
+					break;
+				case 'E':
+					x += mag;
+					break;
+				}//end switch
+			}//end for
+		}//end while pos
+
+		double distance = sqrt(x * x + y * y);
+		printf("Map #%d\n", mapNum++);
+		printf("The treasure is located at (%.3f, %.3f).\n", x, y);
+		printf("The distance to the treasure is %.3f.\n\n", distance);
+		getline(cin, input, '.');
 	}
 }
