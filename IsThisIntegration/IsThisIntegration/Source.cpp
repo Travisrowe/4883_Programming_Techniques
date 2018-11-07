601f0c44f0f2c70fcdf314705938f3e8e249e621
Travisrowe <travisrowe18@gmail.com>
2018-11-06 19:33:27 -0600

    Treasures

--- IsThisIntegration/IsThisIntegration/Source.cpp
+++ IsThisIntegration/IsThisIntegration/Source.cpp
@@ -1,5 +1,6 @@
 //IsThisIntegration.cpp : UVa 10209 : Travis Rowe : Thiefone18 : 11/5/18
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 #define PI 3.14159265
@@ -13,12 +14,12 @@ int main()
 
 		//calculate center, striped area
 		//find area of square + area of slivers outside the square
-		long double FE = 2 * (a * sin(15 * PI / 180));
+		long double FE = 2.0 * (a * sin(30.0 * PI / 180.0));
 		long double centerSquareArea = FE * FE;
 		long double coneBEFArea = PI * a * a * (30.0 / 360.0);
 
 		//Heron's Rule
-		long double s = (2 * a + FE) / 2;
+		long double s = (2.0 * a + FE) / 2.0;
 		long double triangleBEFArea = sqrt(s * (s - FE) * (s - a) * (s - a));
 		long double centerSliversArea = (coneBEFArea - triangleBEFArea);
 		long double centerArea = centerSquareArea + centerSliversArea * 4;
@@ -27,21 +28,24 @@ int main()
 		//find (area of triangle - area of inner sliver) + area of slivers outside of triangle
 		//Heron's Rule
 		//triangle DEF is equilateral
-		s = (3 * FE) / 2;
+		s = (3.0 * FE) / 2.0;
 		long double triangleDEFArea = sqrt(s * (s - FE) * (s - FE) * (s - FE)); 
 		//area of outer slivers is equal to area of center slivers
 		long double DEFOuterSlivers = centerSliversArea;
-		long double dottedArea = (triangleDEFArea + DEFOuterSlivers) * 4;
+		long double dottedArea = (triangleDEFArea + DEFOuterSlivers) * 4.0;
 
 		//calculate grid region
 		//find area of triangle - area of slivers inside triangle
 		//Heron's rule again
 		//triangle DFC is isosceles
-		s = (2 * FE + a) / 2;
+		s = (2.0 * FE + a) / 2.0;
 		long double triangleDFCArea = sqrt(s * (s - FE) * (s - FE) * (s - a));
 		//the same slivers we added to the dotted region, we subtract here
-		long double gridArea = (triangleDFCArea - 2 * DEFOuterSlivers) * 4;
+		long double gridArea = (triangleDFCArea - 2 * DEFOuterSlivers) * 4.0;
 
-		printf("%.3f %.3f %.3f\n", centerArea, dottedArea, gridArea);
+		cout << fixed;
+		cout << setprecision(3) << centerArea << ' ';
+		cout << dottedArea << ' ';
+		cout << gridArea << '\n';
 	}
 }
--- TheresTreasureEverywhere/TheresTreasureEverywhere/Source.cpp
+++ TheresTreasureEverywhere/TheresTreasureEverywhere/Source.cpp
@@ -5,57 +5,74 @@
 #include <sstream>
 using namespace std;
 
+bool IsDigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+bool IsAlpha(char c)
+{
+	return (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z');
+}
+
 int main()
 {
 	string input;
 	int mapNum = 1;
 	const string DELIM = ",";
-	getline(cin, input, '.'); //input is the full line minus the period
+	getline(cin, input); //input is the full line minus the period
 	while (input != "END")
 	{
 		size_t pos = input.find(DELIM);
 		double x = 0;
 		double y = 0;
-		double mag; //magnitude
-		string dir; //direction
-		while (input != "")
-		{
-			stringstream ss;
-			string token = input.substr(0, pos);
-			if (pos + 1 < input.length())
-				input = input.substr(pos + 1);
-			else //this is the last direction
-				input = "";
-			ss << token;
-			
-			ss >> mag >> dir;
+		double mag = 0; //magnitude
+		string dir = ""; //direction
 
-			if (dir.length() > 1) //45 degree cardinal direction
-				mag = sqrt((mag * mag) / 2); //Pytharogm's theorem
-			for (int i = 0; i < dir.length(); i++)
+		for (auto c : input)
+		{
+			//input is number followed by letters followed by comma
+			if (IsDigit(c))
 			{
-				switch (dir[i])
+				mag = mag * 10 + c - 48;
+			}
+			else if (IsAlpha(c))
+			{
+				dir = dir + c;
+			}
+			else //either a comma or a period
+			{
+				if (dir.length() > 1) //45 degree cardinal direction
+					mag = sqrt((mag * mag) / 2); //Pytharogm's theorem
+				for (int i = 0; i < dir.length(); i++)
 				{
-				case 'N':
-					y += mag;
-					break;
-				case 'S':
-					y -= mag;
-					break;
-				case 'W':
-					x -= mag;
-					break;
-				case 'E':
-					x += mag;
-					break;
-				}//end switch
-			}//end for
-		}//end while pos
+					switch (dir[i])
+					{
+					case 'N':
+						y += mag;
+						break;
+					case 'S':
+						y -= mag;
+						break;
+					case 'W':
+						x -= mag;
+						break;
+					case 'E':
+						x += mag;
+						break;
+					}//end switch
+				}//end for
+
+				//reset mag and dir for next set of directions
+				mag = 0;
+				dir = "";
+			}//end else
+		}
 
 		double distance = sqrt(x * x + y * y);
 		printf("Map #%d\n", mapNum++);
 		printf("The treasure is located at (%.3f, %.3f).\n", x, y);
 		printf("The distance to the treasure is %.3f.\n\n", distance);
-		getline(cin, input, '.');
+		getline(cin, input);
 	}
 }
