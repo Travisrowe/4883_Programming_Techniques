c79b38c6cf522f782233afb2f1e77096db033313
Travisrowe <travisrowe18@gmail.com>
2018-12-04 19:26:21 -0600

    theseus

--- TheseusAndTheMinotaur/TheseusAndTheMinotaur/Source.cpp
+++ TheseusAndTheMinotaur/TheseusAndTheMinotaur/Source.cpp
@@ -3,22 +3,56 @@
 //This is an adjacency graph problem
 
 #include <vector>
+#include <bitset>
 #include <string>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	string inp;
-	cin >> inp; //inp is the line of input up to the period
-	while (inp != "#")
+	char inp;
+	cin >> inp; //inp is the first char of the line
+	while (inp != '#')
 	{
-		vector<vector<char>> graph;
-		for (auto c : inp) //iterate a char through inp
+		vector<vector<char>> graph(26); //allocate one space for every letter
+		char vertex = inp;
+		while (vertex != '.')
 		{
-			if (c - 65 >= 0 && c - 65 <= 26) //this is an alpha character
+			char edge;
+			cin >> edge >> edge; //edge is the first node that vertex is connected to
+			while (edge != ';' && edge != '.')
+			{
+				graph[vertex - 65].push_back(edge);
+				cin >> edge;
+			}
+			if (edge != '.')
+				cin >> vertex; //continue the loop
+			else
+				vertex = edge; //exit the loop
+		}//end while(c !- '.')
+		char minot, theseus;
+		int k;
+		cin >> minot >> theseus >> k;
 
-		} //graph is read in, now read in start positions and int k
-		cin >> inp;
+		bitset<26> litRooms;
+		int count = 0;
+		bool trapped = false;
+		while (!trapped)
+		{
+			theseus = minot; //theseus chases the minotaur
+			//minotaur runs from theseus
+			char nextRoom = '.';
+			while (nextRoom == '.' && !trapped)
+			{
+				//find next room for minotaur to run
+
+			}
+			if (count % k == 0 && count != 0)
+				litRooms[theseus - 65] = 1; //light theseus's current room
+		}
+		//while(not trapped)
+			//chase and light
+
+		cin >> inp; //start next line
 	}//end while(inp != #)
 }
